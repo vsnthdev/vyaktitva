4d2426694a460d53105d9e6a5a34f11a73398069
Vasanth Developer <vsnthdev@gmail.com>
2021-12-13 07:45:37 +0530

    🚀 v0.0.3

---
+++ dist/esm/loader.js
@@ -0,0 +1,17 @@
+import { p as promiseResolve, b as bootstrapLazy } from './index-c43bfec8.js';
+
+/*
+ Stencil Client Patch Esm v2.11.0 | MIT Licensed | https://stenciljs.com
+ */
+const patchEsm = () => {
+    return promiseResolve();
+};
+
+const defineCustomElements = (win, options) => {
+  if (typeof window === 'undefined') return Promise.resolve();
+  return patchEsm().then(() => {
+  return bootstrapLazy([["v-header",[[0,"v-header",{"brand":[1],"open":[32]}]]]], options);
+  });
+};
+
+export { defineCustomElements };
---
+++ dist/esm/vyaktitva.js
@@ -0,0 +1,17 @@
+import { p as promiseResolve, b as bootstrapLazy } from './index-c43bfec8.js';
+
+/*
+ Stencil Client Patch Browser v2.11.0 | MIT Licensed | https://stenciljs.com
+ */
+const patchBrowser = () => {
+    const importMeta = import.meta.url;
+    const opts = {};
+    if (importMeta !== '') {
+        opts.resourcesUrl = new URL('.', importMeta).href;
+    }
+    return promiseResolve(opts);
+};
+
+patchBrowser().then(options => {
+  return bootstrapLazy([["v-header",[[0,"v-header",{"brand":[1],"open":[32]}]]]], options);
+});
